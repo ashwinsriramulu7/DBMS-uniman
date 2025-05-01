29e00467c43db810db7100bc419ad03a0f2b4f32
Ashwin Sriramulu <ashwinsriramulu7@gmail.com>
2025-05-01 15:28:49 +0530

    Created includes/db.go that mangages the database connection.Also created go.mod (by running go mod init)

---
+++ includes/db.go
@@ -0,0 +1,35 @@
+package includes
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func InitDB() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("DB connection failed:", err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal("DB ping failed:", err)
+	}
+	fmt.Println("Connected to MySQL!")
+	return db
+}
+
