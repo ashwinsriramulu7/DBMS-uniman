45c3444db46a05ea67d2a9d808004c81925b7775
Ashwin Sriramulu <ashwinsriramulu7@gmail.com>
2025-05-03 12:42:28 +0530

    updated mysqldump file datadir/schema.sql

--- datadir/schema.sql
+++ datadir/schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE `college` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `location` varchar(100) DEFAULT NULL,
-  `estd.` year DEFAULT NULL,
+  `estd` year DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -255,4 +255,4 @@ CREATE TABLE `teaches` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-01 14:29:48
+-- Dump completed on 2025-05-03 12:41:07
---
+++ modules/college.go
@@ -0,0 +1,10 @@
+package modules
+import(
+	"fmt"
+	"log"
+	"github.com/ashwinsriramulu7/DBMS-uniman/includes"
+type college struct { id int; name string; location string; estd int; }
+func CreateCollege(){
+	db := InitDB();
+	db := 
+}
